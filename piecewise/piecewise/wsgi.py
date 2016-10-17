10ee8764ab59ca2b4356cfef2504d3eeeef86f23
Chris Ritzo <critzo@opentechinstitute.org>
2016-10-17 19:40:34 -0400

    added hello world app route to test uwsgi

--- piecewise/piecewise/wsgi.py
+++ piecewise/piecewise/wsgi.py
@@ -19,6 +19,10 @@ import pprint
 import piecewise.config
 config = piecewise.config.read_system_config()
 
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    return hello_world()
+
 @app.route("/q/<aggregation>")
 def query_statistics(aggregation):
     filters = dict()
